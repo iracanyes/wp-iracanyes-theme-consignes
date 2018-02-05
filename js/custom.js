dc8f712c1154ed788ce35c4a3dfc751e1094a684
iracanyes <k.iracanye@gmail.com>
2018-02-05 04:21:17 +0100

    Commit final

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,21 @@
-$(function () {
+$(function ()  {
+    // Correction erreur allow="autoplay; encrypted-media"
     $("iframe").attr("allow","encrypted-media");
+
+    // Action autoplay video
    $("#overlay-video").one("mousedown", function () {
+
+       // chargement source iframe
        var src = $("iframe").attr('src') ;
        console.log(src);
+
+       // ajout attribut autoplay
        src = src+"&autoplay=1";
        console.log(src);
+
+       // placement du nouvel attribut source dans la balise iframe
        $("#thevideo>iframe").attr('src', src );
        $("#thevideo").css("padding-bottom","56.25%");
    });
 });
 
-/*
-$("#thevideo>iframe").attr('src', $("iframe").attr('src') );
- */
